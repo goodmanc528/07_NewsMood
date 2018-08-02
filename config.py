e42ea2a4feda063d7b9fd0b7086320bbb75c6024
Clint Goodman <goodmanc528@gmail.com>
2018-08-01 19:03:06 -0500

    initial commit

---
+++ config.py
@@ -0,0 +1,11 @@
+# Twitter API Keys
+# consumer_key = "Ed4RNulN1lp7AbOooHa9STCoU"
+# consumer_secret = "P7cUJlmJZq0VaCY0Jg7COliwQqzK0qYEyUF9Y0idx4ujb3ZlW5"
+# access_token = "839621358724198402-dzdOsx2WWHrSuBwyNUiqSEnTivHozAZ"
+# access_token_secret = "dCZ80uNRbFDjxdU2EckmNiSckdoATach6Q8zb7YYYE5ER"
+
+# My Twitter API Keys....
+consumer_key = '0Pvp4m5QkEgV06EuWc9iIctA5'
+consumer_secret = 'hH6aGHwSYgUWFWb1fGbWntA4K5c3lFVluxLkFrF6yH37Ne4rM2'
+access_token = '214972054-Lo0fQL5ogqCEwakt9WOmRGRYWO2zfQHxavkwP1N1'
+access_token_secret = 'Kda9kitmLFhkj8tHiKZx3oeHMY7df5YNcJMTnBuE37vY9'
